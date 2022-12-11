910f80e3b162c0b0c4496afad377b84100939e86
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-12-11 17:00:06 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
+#include "analyze_value_range.h"
+#include "checker.h"
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   if(temperature < 0 || temperature > 45) {
