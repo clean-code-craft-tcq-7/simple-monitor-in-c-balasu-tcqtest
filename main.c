340123a9d7acf1c7b873e0eca6cdab671ae6857c
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-12-11 17:16:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 #include "checker.h"
+#include "analyze_value_range.h"
 
 int main() {
+  // Test for temperature, soc, charge current in range
   assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  // Test for temperature not in Range, soc and charge current in range
+  assert(!batteryIsOk(50, 70, 0.5));
+  // Test for temperature, charge current in range and soc out of range
+  assert(!batteryIsOk(30, 85, 0.5));
+  // Test for temperature, soc in range and charge current out of range
+  assert(!batteryIsOk(30, 70, 0.9));
 }
