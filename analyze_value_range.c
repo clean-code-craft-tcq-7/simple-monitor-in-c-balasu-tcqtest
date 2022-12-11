ccab97186ef067b384b14de773afaf25d26b51d5
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-12-11 16:57:11 +0530

    Create analyze_value_range.c

---
+++ analyze_value_range.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "analyze_value_range.h"
+
+void printError(char* errStr)
+{
+  printf("%s", errStr);
+}
+
+int analyzeTemperatureInRange(float temprature)
+{
+  int retStatus = 1;
+  if(temperature < 0 || temperature > 45) 
+  {
+    printError("Temperature out of range!\n");
+    retStatus = 0;
+  }
+  return retStatus;
+}
+
+int analyzeSocInRange(float soc)
+{
+  int retStatus = 1;
+  if(soc < 20 || soc > 80)
+  {
+    printError("State of Charge out of range!\n");
+    retStatus = 0;
+  }
+  return retStatus;
+}
+
+int analyzeChargeRateInRange(float chargeRate)
+{
+  int retStatus = 1;
+  if(chargeRate > 0.8)
+  {
+    printError("Charge Rate out of range!\n");
+    retStatus = 0;
+  }
+  return retStatus;
+}
