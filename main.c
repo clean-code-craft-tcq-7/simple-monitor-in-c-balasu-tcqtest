737c5872ba540f7c6184293515c22b23f7919f89
APAC\bki8cob <balasubramanian.krishnamoorthy@in.bosch.com>
2022-12-20 12:05:43 +0530

    Added test cases for battery status api

--- main.c
+++ main.c
@@ -11,13 +11,38 @@ int main() {
   setPrintLanguage(ENGLISH);
   // Test for temperature, soc, charge current in range
   assert(batteryIsOk(25, 70, 0.7, &printToConsole));
-  // Test for temperature not in Range, soc and charge current in range
+
+  // Test for temperature less than min, soc and charge current in range
   assert(!batteryIsOk(8, 70, 0.5,&printToConsole));
-  // Test for temperature, charge current in range and soc out of range
-  assert(!batteryIsOk(30, 85, 0.5, &printToConsole));
-  // Test for temperature, soc in range and charge current out of range
-  assert(!batteryIsOk(30, 70, 0.9, &printToConsole));
+  // Test for temperature greater than max, soc and charge current in range
+  assert(!batteryIsOk(46, 70, 0.5,&printToConsole));
+
+  // Test for soc less than min and temperature, charge current in range
+  assert(!batteryIsOk(25, 10, 0.5,&printToConsole));
+  // Test for soc greater than max and temperature, charge current in range
+  assert(!batteryIsOk(25, 81, 0.5,&printToConsole));
+
+  // Test for charge current greater than max and temperature,soc  in range
+  assert(!batteryIsOk(25, 70, 0.9,&printToConsole));
+
+  // Temperature test with warning enabled
+  setWarningSupport(TEMPERATURE_ANALYSIS, 1);
+  // Test for temperature greater than min and less than tolerance, soc and charge current in range
+  assert(batteryIsOk(11.5, 70, 0.5,&printToConsole));
+  // Test for temperature greater than tolerance and less than max, soc and charge current in range
+  assert(batteryIsOk(44.5, 70, 0.5,&printToConsole));
+
+  // SOC test with warning enabled
+  setWarningSupport(SOC_ANALYSIS, 1);
+  // Test for SOC greater than min and less than tolerance, temperature and charge current in range
+  assert(batteryIsOk(25, 22, 0.5,&printToConsole));
+  // Test for SOC greater than tolerance and less than max, temperature and charge current in range
+  assert(batteryIsOk(25, 77.5, 0.5,&printToConsole));
+
+  // SOC test with warning enabled
   setWarningSupport(CHARGE_RATE_ANALYSIS, 1);
-  assert(batteryIsOk(25, 70, 0.79, &printToConsole));
+  // Test for charge current greater than tolerance and less than max, temperature and SOC in range
+  assert(batteryIsOk(25, 50, 0.787,&printToConsole));
+
   return 0;
 }
